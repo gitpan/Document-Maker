a3013d3e7446af09c14509429c6a44b6bd3f4fe1
Robert Krimen <rkrimen@cpan.org>
2007-11-20 22:42:24 +0000

    Import of RKRIMEN/Document-Maker-0.021 from CPAN.
    gitpan-cpan-distribution: Document-Maker
    gitpan-cpan-version:      0.021
    gitpan-cpan-path:         RKRIMEN/Document-Maker-0.021.tar.gz
    gitpan-cpan-author:       RKRIMEN
    gitpan-cpan-maturity:     released

--- lib/Document/Maker.pm
+++ lib/Document/Maker.pm
@@ -9,11 +9,11 @@ Document::Maker - Makefile-like functionality in Perl
 
 =head1 VERSION
 
-Version 0.020
+Version 0.021
 
 =cut
 
-our $VERSION = '0.020';
+our $VERSION = '0.021';
 
 =head1 SYNOPSIS
 
